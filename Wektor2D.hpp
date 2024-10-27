860acf11ef4bd8f476471661d9bc6090b7c4e785
Kayushhh <czesiuleczek4@gmail.com>
2024-10-27 16:14:58 +0100

    Lab 1
    Second attempt at the test, had to delete the initial repository, since I might've uploaded too many unnecessary files.

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,24 @@
 class Wektor2D
 {
-    // Tutaj napisz implementacje klasy Wektor2D
+    private:
+    double x_cor;
+    double y_cor;
+
+    public:
+
+    Wektor2D() : x_cor(0), y_cor(0) {} //Konstruktor domyslny
+    Wektor2D(double x, double y) : x_cor(x), y_cor(y) {}
+    double getX() const {return x_cor;}
+    double getY() const {return y_cor;}
+    void setX(double x) {x_cor = x;}
+    void setY(double y) {y_cor=y;}
+    Wektor2D operator+(const Wektor2D& W2) const //Operator dodawania wektorów
+    {
+        return Wektor2D(x_cor+W2.x_cor, y_cor+W2.y_cor);
+    }
+    double operator*(const Wektor2D& W2) const //iloczyn skalarny
+    {
+        return (x_cor * W2.x_cor) + (y_cor * W2.y_cor);
+    }
+
 };
